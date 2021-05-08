83a499fd7dbc01cccee2c125c5deb8d58ad9c13d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-05-08 14:20:37 -0400

    refactor: different index export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import countries from "./source/countries.json";
-import languages from "./source/languages.json";
-import { Country } from "./types/question-source";
-import { QuestionGenerator } from "./utils/question-generator";
-
-export const countryQuizGenerator = QuestionGenerator({
-  countries: countries as Country[],
-  languages,
-});
+import { countryQuiz } from "./utils/question-generator";
+export const { generateQuiz, ...questionGenerators } = countryQuiz();
