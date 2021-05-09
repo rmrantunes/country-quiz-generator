7740100c2afbf0bd62fd0e94bd2267a4fb231ed8
Rafael Antunes <rafaelmessias710@gmail.com>
2021-05-09 09:11:50 -0400

    feat: new dist declaration file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,56 @@
-export declare const generateQuiz: (amount: number, options?: import("./types/question").GenerateQuizOptions<"whichCountryForGivenCapital" | "whichCountryForGivenFlag" | "whichCountryForGivenLanguage"> | undefined) => import("./types/question").Question[], questionGenerators: {
-    whichCountryForGivenCapital: () => import("./types/question").Question;
-    whichCountryForGivenFlag: () => import("./types/question").Question;
-    whichCountryForGivenLanguage: () => import("./types/question").Question;
+export interface Language {
+  name: string;
+  code: string;
+}
+
+export interface Continent {
+  name: string;
+}
+
+export interface Country {
+  name: string;
+  capital: string;
+  code: string;
+  continent: Continent;
+  languages: Language[];
+}
+
+export interface MappedCountry {
+  name: string;
+  code?: string;
+  capital?: string;
+}
+
+export interface Source {
+  countries: Country[];
+  languages: Language[];
+}
+
+export type QuestionType =
+  | "whichCountryForGivenCapital"
+  | "whichCountryForGivenFlag"
+  | "whichCountryForGivenLanguage";
+
+export interface Question {
+  type: QuestionType;
+  flagSrc?: string;
+  title: string;
+  correctAnswer: string;
+  options: string[];
+}
+
+export interface GenerateQuizOptions<K> {
+  questionTypesToSelect?: K[];
+  questionTypesToExclude?: K[];
+}
+
+export function generateQuiz(
+  amount: number,
+  options?: GenerateQuizOptions<QuestionType> | undefined
+): Question[];
+
+export const questionGenerators: {
+  whichCountryForGivenCapital: () => Question;
+  whichCountryForGivenFlag: () => Question;
+  whichCountryForGivenLanguage: () => Question;
 };
